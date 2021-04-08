da41ad247c4eb043cb6eb5ae7a45c73ed1524a2b
kula <kula@live.com>
2021-04-08 18:27:24 +0800

    feat: code structure

---
+++ src/main/java/io/vertx/elasticsearch/client/Elasticsearch.java
@@ -0,0 +1,4 @@
+package io.vertx.elasticsearch.client;
+
+public class Elasticsearch {
+}
---
+++ src/main/java/io/vertx/elasticsearch/client/Request.java
@@ -0,0 +1,7 @@
+package io.vertx.elasticsearch.client;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.VertxGen;
+
+public class Request {
+}
